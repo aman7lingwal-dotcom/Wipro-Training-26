1366741cb76c582057b21e12d04827532c5454e0
Aman lingwal <aman7lingwal@gmail.com>
2026-02-12 11:00:58 +0530

    Added TASK1

---
+++ TASK1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace day2
+{
+    internal class Task1
+    {
+        public void Rev() {
+            string Sen = "Welcome to c# program by wipro";
+            string[] words = Sen.Split();
+            for(int i = 0; i < words.Length; i++) {
+                if (i % 2 != 0) {
+                    StringBuilder sb = new StringBuilder();
+                    for(int j = words[i].Length - 1; j >= 0; j--) {
+                        sb.Append(words[i][j]);
+                    }
+                    words[i] = sb.ToString();
+
+
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", words));
+
+
+        }
+        static void Main() {
+            Task1 r = new Task1();
+            r.Rev();
+        }
+        }
+
+
+
+   
+    }
+
