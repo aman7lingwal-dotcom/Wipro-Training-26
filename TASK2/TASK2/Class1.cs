c185a78beb2c7e0949d3805fef30b7396d87cb86
Aman lingwal <aman7lingwal@gmail.com>
2026-02-12 11:38:17 +0530

    Re-added TASK2 correctly

---
+++ TASK2/TASK2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter sentence:");
+        string input = Console.ReadLine().ToLower().Replace(" ", "");
+
+        string reversed = "";
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reversed += input[i];
+        }
+
+        Console.WriteLine(input == reversed ? "Palindrome" : "Not Palindrome");
+    }
+}
