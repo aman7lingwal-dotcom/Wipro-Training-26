989787db30805f67ccde4446c21434126a8dca6b
Aman lingwal <aman7lingwal@gmail.com>
2026-02-13 10:22:54 +0530

    character search

---
+++ Day5TestCode1/Day5TestCode1/Charhelp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Charhelp
+{
+    // Method to check the non repeat
+    public static char Firstnonreapeat(string S)
+    {
+        int[] arr = new int[26]; // array for storing the freq
+        foreach (char c in S)
+        {
+            arr[c - 'a']++; // since it will be something like c-a == (99-97)=2,i.e., index 2.
+
+        }
+        // traversing and storing has been done
+        foreach (char c in S)
+        { // traverse once more 
+            if (arr[c - 'a'] == 1) // as soon as value at index=1 is found return
+                return c;
+        }
+        return '$';
+    }
+}
---
+++ Day5TestCode1/Day5TestCode1/Main.cs
@@ -0,0 +1,16 @@
+﻿// Main file
+
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the string: ");
+        string word = Console.ReadLine()!;
+        char result = Charhelp.Firstnonreapeat(word);
+        if (result == '$')
+            Console.WriteLine("All characters repeat: " + result);
+        else
+            Console.WriteLine("The first Non-repeating character is: " + result);
+    }
+}
