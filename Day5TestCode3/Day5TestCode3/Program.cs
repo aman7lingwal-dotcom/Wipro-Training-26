f7e0c4f7ab505544bd24a788ddb2945c7c01f291
Aman lingwal <aman7lingwal@gmail.com>
2026-02-13 12:40:25 +0530

    Day5 Code 3 for practice

---
+++ Day5TestCode3/Day5TestCode3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class ReduceString
+{
+    public static string Reduce(string str, int k)
+    {
+        char[] result = new char[str.Length]; // store final characters
+        int[] count = new int[str.Length];    // store counts
+        int j = 0; // acts like stack top
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            result[j] = str[i];
+
+            // if same as previous character
+            if (j > 0 && result[j] == result[j - 1])
+                count[j] = count[j - 1] + 1;
+            else
+                count[j] = 1;
+
+            // if count reaches k → remove k characters
+            if (count[j] == k)
+                j = j - k; // remove last k chars
+
+            j++;
+        }
+
+        // build final string
+        return new string(result, 0, j);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Reduce("geeksforgeeks", 2)); // gksforgks
+        Console.WriteLine(Reduce("qddxxxd", 3));       // q
+    }
+}
