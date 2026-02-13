275d405e0c67eec3c12bf8b2194d0ac953cf94b3
Aman lingwal <aman7lingwal@gmail.com>
2026-02-13 11:16:02 +0530

    palindrome sentence

---
+++ Day5Task2/Day5Task2/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string s;
+        Console.WriteLine("enter the string: ");
+        s = Console.ReadLine()!;
+        bool result = Palindrome.Ispalindrome(s);
+        if (result)
+            Console.WriteLine("Yes it is palindrome");
+        else
+            Console.WriteLine("No it is not a palindrome");
+
+    }
+
+}
---
+++ Day5Task2/Day5Task2/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+public class Palindrome
+{
+    public static bool Ispalindrome(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in s)
+        {
+            if (char.IsLetterOrDigit(c))
+                sb.Append(Char.ToLower(c));
+        }
+        string newstr = sb.ToString();
+
+        int l = 0;
+        int r = newstr.Length - 1;
+        while (l < r)
+        {
+            if (newstr[l] != newstr[r])
+                return false;
+            l++;
+            r--;
+
+
+        }
+
+        return true;
+
+
+
+
+    }
+}
