987bd3c005722cad8618892f63a356e5eb0f86d8
Aman lingwal <aman7lingwal@gmail.com>
2026-03-17 20:16:29 +0530

    Remove from repo but keep local

--- Connect_plus/backend/ConnectPlus.API/Repositories/Interfaces/ITicketStatusRepository.cs
+++
@@ -1,9 +0,0 @@
-using ConnectPlus.API.Models;
-
-namespace ConnectPlus.API.Repositories.Interfaces
-{
-    public interface ITicketStatusRepository
-    {
-        Task<IEnumerable<TicketStatus>> GetAllAsync();
-    }
-}
--- Connect_plus/backend/ConnectPlus.API/Services/Interfaces/ITicketStatusService.cs
+++
@@ -1,9 +0,0 @@
-using ConnectPlus.API.Models;
-
-namespace ConnectPlus.API.Services.Interfaces
-{
-    public interface ITicketStatusService
-    {
-        Task<IEnumerable<TicketStatus>> GetAllStatusesAsync();
-    }
-}
